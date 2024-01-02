d1703f9ac39b881b0cec16423f47cbc9100034c9
Jeimnis_Bermejo <jeimnisbermejo@gmail.com>
2024-01-02 18:04:39 -0500

    Sprint 8

---
+++ build/resources/main/static/web/js/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export const theme = {
+    extend: {
+      colors: {
+        'greenDark':' #042326',
+        'beige':'#DFD9AD',
+        clifford: '#da373d'
+      },
+        
+      }
+    }
+  
+ 
